e6bde1117509fb5696de91309abcb3e2ff44d554
Mart Jansink <mart@gofilex.nl>
2020-06-01 17:54:49 +0200

    Optionally set the readableAll and writableAll options on a Unix socket

--- packages/webapp/webapp_server.js
+++ packages/webapp/webapp_server.js
@@ -1149,7 +1149,11 @@ function runWebAppServer() {
     if (unixSocketPath) {
       // Start the HTTP server using a socket file.
       removeExistingSocketFile(unixSocketPath);
-      startHttpServer({ path: unixSocketPath });
+      startHttpServer({
+        path: unixSocketPath,
+        readableAll: (process.env.UNIX_SOCKET_READABLE_ALL === "true"),
+        writableAll: (process.env.UNIX_SOCKET_WRITABLE_ALL === "true"),
+      });
       registerSocketFileCleanup(unixSocketPath);
     } else {
       localPort = isNaN(Number(localPort)) ? localPort : Number(localPort);
